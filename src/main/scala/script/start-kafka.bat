d433420a2ac09a198cec36e8f0f1b0a9c03c10a3
IzzAl-Deen <s12324586@stu.najah.edu>
2025-12-18 18:51:04 +0200

    added a script runner to run kafka faster

---
+++ src/main/scala/script/start-kafka.bat
@@ -0,0 +1,31 @@
+@echo off
+
+
+set PROJECT_ROOT=%~dp0..
+set DATA_DIR=%PROJECT_ROOT%\kafka-data
+
+
+set KAFKA_HOME=C:\kafka_2.12-2.4.0
+
+echo Starting Kafka using:
+echo Kafka home: %KAFKA_HOME%
+echo Kafka data: %DATA_DIR%
+
+
+if not exist "%DATA_DIR%" (
+    mkdir "%DATA_DIR%"
+)
+
+
+start "Zookeeper" cmd /k ^
+"%KAFKA_HOME%\bin\windows\zookeeper-server-start.bat ^
+%KAFKA_HOME%\config\zookeeper.properties"
+
+
+timeout /t 5 > nul
+
+
+start "Kafka" cmd /k ^
+"%KAFKA_HOME%\bin\windows\kafka-server-start.bat ^
+%KAFKA_HOME%\config\server.properties ^
+--override log.dirs=%DATA_DIR%"
