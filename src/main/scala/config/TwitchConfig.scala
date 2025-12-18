610d92b048efce3cf3cd88005e5f976e41a05ec7
IzzAl-Deen <s12324586@stu.najah.edu>
2025-12-18 16:49:22 +0200

    added TwitchConfig and KafkaConfig

---
+++ src/main/scala/config/TwitchConfig.scala
@@ -0,0 +1,9 @@
+package config
+
+object TwitchConfig {
+  val server = "irc.chat.twitch.tv"
+  val port = 6667
+  val nickname = "abdallah2_"
+  val token = "oauth:gtgi7x4j6kabgyzi24r05h72dpvqf6"
+  val channel = "#mooda"
+}
