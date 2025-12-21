04026a281ea83d61cca0969c57afe9302905c15b
IzzAl-Deen <s12324586@stu.najah.edu>
2025-12-21 17:11:06 +0200

    Added MongoConfig and CMS for topUsers

--- src/main/scala/config/TwitchConfig.scala
+++ src/main/scala/config/TwitchConfig.scala
@@ -4,6 +4,6 @@ object TwitchConfig {
   val server = "irc.chat.twitch.tv"
   val port = 6667
   val nickname = "abdallah2_"
-  val token = "oauth:35dxj6e2y0mpow3kefx7m22c5iptxu"
+  val token = "oauth:3rhgvhgv0vn2731ilfgd3zw14j94et"
   val channel = "#mooda"
 }
---
+++ src/main/scala/consumer/helper/OutputToConsole.scala
@@ -0,0 +1,21 @@
+package consumer.helper
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.streaming.StreamingQuery
+
+object OutputToConsole {
+  def Save(DF : DataFrame, formatType : String): StreamingQuery ={
+    val windowQuery = DF.writeStream
+      .outputMode(formatType)
+      .format("console")
+      .option("truncate", false)
+      .option("numRows", 15)
+      .trigger(Trigger.ProcessingTime("15 seconds"))
+      .start()
+
+    windowQuery
+
+  }
+
+}
